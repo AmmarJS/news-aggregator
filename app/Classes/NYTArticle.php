63ebd227270f3273d39a045c2d33e87170019afa
AmmarJS <ammar552259@gmail.com>
2023-10-24 19:53:35 +0300

    updated readme

--- app/Classes/NYTArticle.php
+++ app/Classes/NYTArticle.php
@@ -15,7 +15,6 @@ class NYTArticle extends Article {
         $this->category = $article->section ?? "General";
         $this->date = Carbon::parse($article->published_date)->setTimezone('UTC')->format('Y-m-d H:i:s');;
         $this->description = $article->description ?? "No description";
-        // TODO: determine how to get the thumbnail
         $this->image = $article->fields->thumbnail ?? null;
         $this->source = $article->source ?? "New York Times";
         $this->title = $article->title ?? "No title";
